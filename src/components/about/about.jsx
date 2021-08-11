598957b42c1d5f8b9574429363cc72df23e1ad43
MasonYageman <masonyageman@gmail.com>
2021-08-11 17:29:08 -0400

    download cv not working

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -28,11 +28,11 @@ const Header = (props) =>{
                   </p>
                 </div>
                 <div className="column w-1000-stack">
-                  <form method="get" action="file.doc">
-                    <button type="submit">Download CV <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 15" style={{fill: 'rgba(0, 0, 0, 1)', transform: '', msFilter: ''}}><path d="M12 16L16 11 13 11 13 4 11 4 11 11 8 11z" /><path d="M20,18H4v-7H2v7c0,1.103,0.897,2,2,2h16c1.103,0,2-0.897,2-2v-7h-2V18z" /></svg></button>
-                  </form>
-                    
-                
+
+                <a href="../src/components/about/Mason_Yageman_Resume_August.pdf" download="Mason_Yageman_Resume" target="_blank" className="btn btn--download">
+                    <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" style={{fill: 'rgba(0, 0, 0, 1)', transform: '', msFilter: ''}}><path d="M12 16L16 11 13 11 13 4 11 4 11 11 8 11z" /><path d="M20,18H4v-7H2v7c0,1.103,0.897,2,2,2h16c1.103,0,2-0.897,2-2v-7h-2V18z" /></svg>
+                    Download CV
+                  </a>
                 </div>
               </div>
             </div>
